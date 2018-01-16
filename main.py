a546e121e3ca274b5431602c5e769e7009c40861
alon0 <alonmor89@gmail.com>
2018-01-16 17:13:00 +0200

    almost done with 1

--- main.py
+++ main.py
@@ -1,82 +1,88 @@
-import sklearn.datasets as sk
 import pandas as pd;
-from time import time
-from sklearn.linear_model import SGDClassifier
-from sklearn.linear_model import Perceptron
-from sklearn.naive_bayes import MultinomialNB
-from sklearn import metrics
-from sklearn import preprocessing
 import re
+import matplotlib
 
 corpus_path = 'D:\gender-classifier-DFE-791531.csv'
 data = pd.read_csv(corpus_path)
 
 print('data loaded')
-
 print("Part 1 - clean the text")
-from nltk.corpus import stopwords
-from nltk.stem import PorterStemmer
-from nltk.tokenize import word_tokenize
+
+# Define regex consts
+emoticons_str = r"""
+    (?:
+        [:=;] # Eyes
+        [oO\-]? # Nose (optional)
+        [D\)\]\(\]/\\OpP] # Mouth
+    )"""
+regex_str = [
+    emoticons_str,
+    r'<[^>]+>',  # HTML tags
+    r'(?:@[\w_]+)',  # @-mentions
+    r"(?:\#+[\w_]+[\w\'_\-]*[\w_]+)",  # hash-tags
+    r'http[s]?://(?:[a-z]|[0-9]|[$-_@.&amp;+]|[!*\(\),]|(?:%[0-9a-f][0-9a-f]))+',  # URLs
+    # r'(?:(?:\d+,?)+(?:\.?\d+)?)', # numbers
+    # r"(?:[a-z][a-z'\-_]+[a-z])", # words with - and '
+    r'(?:[\w_]+)',  # other words
+    r'(?:\S)'  # anything else
+]
+
+tokens_re = re.compile(r'(' + '|'.join(regex_str) + ')', re.VERBOSE | re.IGNORECASE)
+emoticon_re = re.compile(r'^' + emoticons_str + '$', re.VERBOSE | re.IGNORECASE)
 
 
+def tokenize(s):
+    s = re.sub(r'[^\x00-\x7f]*', r'', s)
+    return tokens_re.findall(s)
 
-def cleaning(s):
-    s = str(s)
-    s = s.lower()
-    s = re.sub('\s\W',' ',s)
-    s = re.sub('\W,\s',' ',s)
-    s = re.sub(r'[^\w]', ' ', s)
-    s = re.sub("\d+", "", s)
-    s = re.sub('\s+',' ',s)
-    s = re.sub('[!@#$_]', '', s)
-    s = s.replace("co","")
-    s = s.replace("https","")
-    s = s.replace(",","")
-    s = s.replace("[\w*"," ")
-    return s
 
-data['Tweets'] = [cleaning(s) for s in data['text']]
-data['Description'] = [cleaning(s) for s in data['description']]
+def preprocess(s):
+    tokens = tokenize(s)
+    # To lower
+    tokens = [token if emoticon_re.search(token) else token.lower() for token in tokens]
+    return tokens
 
-from nltk.corpus import stopwords
-stop = set(stopwords.words('english'))
-data['Tweets'] = data['Tweets'].str.lower().str.split()
-data['Tweets'] = data['Tweets'].apply(lambda x : [item for item in x if item not in stop])
 
-data.gender.value_counts()
+def clean_stopwords(text):
+    from nltk.corpus import stopwords
+    import string
+    # Create stop word dictionary
+    punctuation = list(string.punctuation)
+    stop = stopwords.words('english') + punctuation + ['rt', 'via']
+    no_stopwords_tokens
+    # Remove stop words
+    for token in text:
+        if token not in stop:
+            no_stopwords_tokens.append(token)
+    return no_stopwords_tokens;
 
+
+row_it = data.iterrows()
+test_clean = []
+# Iterate the data
+for i, line in row_it:
+    no_stopwords_tokens = []
+    tokens = preprocess(line['text'])
+    no_stopwords_tokens = clean_stopwords(tokens)
+    test_clean.append(' '.join(no_stopwords_tokens))
+data['text_clean'] = test_clean
+
+# Explore gender distributation count
+print(data.gender.value_counts())
+
+# Explore distributation of words per gender
 Male = data[data['gender'] == 'male']
 Female = data[data['gender'] == 'female']
 Brand = data[data['gender'] == 'brand']
-Male_Words = pd.Series(' '.join(Male['Tweets'].astype(str)).lower().split(" ")).value_counts()[:20]
-Female_Words = pd.Series(' '.join(Female['Tweets'].astype(str)).lower().split(" ")).value_counts()[:20]
-Brand_words = pd.Series(' '.join(Brand['Tweets'].astype(str)).lower().split(" ")).value_counts()[:10]
-
+Male_Words = pd.Series(' '.join(Male['text_clean'].astype(str)).lower().split(" ")).value_counts()[:20]
+Female_Words = pd.Series(' '.join(Female['text_clean'].astype(str)).lower().split(" ")).value_counts()[:20]
+Brand_words = pd.Series(' '.join(Brand['text_clean'].astype(str)).lower().split(" ")).value_counts()[:10]
 
 print(Female_Words)
-
+Female_Words.plot(kind='bar', stacked=True, colormap='OrRd')
 print(Male_Words)
-    # for fileB, target in zip(data.data, data.target):
-    #     # ------------TO LOWER CASE-------------
-    #     file = fileB.decode("utf-8").lower()
-    #     # ------------TOKENIZE-------------
-    #     word_tokens = word_tokenize(file)
-    #     # ------------REMOVE STOP WORDS-------------
-    #     filtered_sentence = [w for w in word_tokens if not w in stop_words]
-    #     filtered_sentence = []
-    #     for w in word_tokens:
-    #         if w not in stop_words:
-    #             filtered_sentence.append(w)
-    #     # ------------STEMMING-------------
-    #     ps = PorterStemmer()
-    #     stemmedFile = []
-    #     for word in filtered_sentence:
-    #         for w in word.split(" "):
-    #             stem = ps.stem(w)
-    #             stemmedFile.append(stem)
-    #             term_per_category[target][word] += 1
-    #     # ------------PUT FILE BACK-------------
-    #     fileB = ' '.join(stemmedFile)
-    #
+Male_Words.plot(kind='bar', stacked=True, colormap='plasma')
+print(Brand_words)
+Brand_words.plot(kind='bar', stacked=True, colormap='Paired')
 
 print("**********FINISHED CLEANING THE TEXT***************")
